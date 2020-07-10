38ee7123ae4325a497a17817a428a65fe69295d1
JAVI <ortiz.javier10@gmail.com>
2020-07-10 18:54:07 -0400

    function declarations

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+var formEl = document.querySelector('#city-search');
+var cityInputEl = document.querySelector('#city-search-input');
+var searchHistoryEl = document.querySelector('#city-history');
+var dayForecastEl = document.querySelector('#current-forecast');
+var cityDateEl = document.querySelector('#city-date');
+var weatherIconEl = document.querySelector('#weather-icon');
+var currentTempEl = document.querySelector('#current-temp');
+var currentHumidityEl = document.querySelector('#current-humidity');
+var currentWindEl = document.querySelector('#current-wind');
+var currentUvEl = document.querySelector('#current-uv-number')
+var currentEvSpan = document.querySelector('#current-uv-span');
+var weekForecastEl = document.querySelector('#week-forecast');
+var weekForcastH2El = document.querySelector('#week-forecast-h2');
+
+
+
+
